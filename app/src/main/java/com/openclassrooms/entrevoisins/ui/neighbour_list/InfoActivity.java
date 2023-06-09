09096788be9da4a5ece337ff54c06b534424a6b7
Emre Turhal <emreturhal9@gmail.com>
2023-06-09 20:04:09 +0200

    restructured InfoActivity code

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/InfoActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/InfoActivity.java
@@ -1,10 +1,9 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,18 +13,18 @@ import com.openclassrooms.entrevoisins.di.DI;
 import com.openclassrooms.entrevoisins.model.Neighbour;
 import com.openclassrooms.entrevoisins.service.NeighbourApiService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class InfoActivity extends AppCompatActivity {
 
-    private ImageView mInfoAvatar;
-    private FloatingActionButton mInfoFavBtn;
-    private TextView mNameText;
-    private TextView mAbout;
-    private TextView mPhone;
-    private TextView mAddress;
-    private TextView mDetailNameText;
+    private Neighbour neighbour;
+    private ImageView infoAvatar;
+    private FloatingActionButton infoFavBtn;
+    private TextView nameText;
+    private TextView about;
+    private TextView phone;
+    private TextView address;
+    private TextView detailNameText;
     
 
     private NeighbourApiService mApiService;
@@ -34,45 +33,41 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        mInfoAvatar = findViewById(R.id.activity_info_avatar);
-        mInfoFavBtn = findViewById(R.id.activity_info_fav_btn);
-        mNameText = findViewById(R.id.activity_info_name_text);
-        mAbout = findViewById(R.id.activity_info_about_text);
-        mPhone = findViewById(R.id.activity_info_phone_text);
-        mAddress = findViewById(R.id.activity_info_address_text);
-        mDetailNameText = findViewById(R.id.activity_detail_name_text);
+        neighbour = (Neighbour) getIntent().getSerializableExtra("Editing");
+        infoAvatar = findViewById(R.id.activity_info_avatar);
+        infoFavBtn = findViewById(R.id.activity_info_fav_btn);
+        nameText = findViewById(R.id.activity_info_name_text);
+        about = findViewById(R.id.activity_info_about_text);
+        phone = findViewById(R.id.activity_info_phone_text);
+        address = findViewById(R.id.activity_info_address_text);
+        detailNameText = findViewById(R.id.activity_detail_name_text);
 
-
-        Neighbour neighbour = (Neighbour) getIntent().getSerializableExtra("Editing");
-
-
-        mNameText.setText(neighbour.getName());
-        mDetailNameText.setText(neighbour.getName());
-        Glide.with(this).load(neighbour.getAvatarUrl()).into(mInfoAvatar);
-        mAbout.setText(neighbour.getAboutMe());
-        mPhone.setText(neighbour.getPhoneNumber());
-        mAddress.setText(neighbour.getAddress());
-
-
-        mApiService = DI.getNeighbourApiService();
-
-
-
-
-        mInfoFavBtn.setOnClickListener(v -> mApiService.addFavorite(neighbour));
-
+        configureNeighbourDetails();
+        configureAddToFavouritesButton();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void configureNeighbourDetails(){
+        nameText.setText(neighbour.getName());
+        detailNameText.setText(neighbour.getName());
+        Glide.with(this).load(neighbour.getAvatarUrl()).into(infoAvatar);
+        about.setText(neighbour.getAboutMe());
+        phone.setText(neighbour.getPhoneNumber());
+        address.setText(neighbour.getAddress());
+    }
+    public void configureAddToFavouritesButton(){
+        mApiService = DI.getNeighbourApiService();
+        infoFavBtn.setOnClickListener(v -> mApiService.addFavorite(neighbour));
     }
 
 
