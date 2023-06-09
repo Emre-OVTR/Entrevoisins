c78b4a4ee733cbec5de979236da90a0924e53312
Emre Turhal <emreturhal9@gmail.com>
2023-06-09 18:30:48 +0200

    changes to app files

--- app/src/main/java/com/openclassrooms/entrevoisins/events/DeleteNeighbourEvent.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/events/DeleteNeighbourEvent.java
@@ -2,20 +2,14 @@ package com.openclassrooms.entrevoisins.events;
 
 import com.openclassrooms.entrevoisins.model.Neighbour;
 
-/**
- * Event fired when a user deletes a Neighbour
- */
-public class DeleteNeighbourEvent {
 
-    /**
-     * Neighbour to delete
-     */
+public class
+DeleteNeighbourEvent {
+
+
     public Neighbour neighbour;
 
-    /**
-     * Constructor.
-     * @param neighbour
-     */
+
     public DeleteNeighbourEvent(Neighbour neighbour) {
         this.neighbour = neighbour;
     }
